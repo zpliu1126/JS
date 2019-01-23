97c1b17e5e05d43c682c2eacc2ed238df9a7d8ca
zpliu1126 <1944532210@qq.com>
2019-01-23 12:57:24 +0800

    note

--- up_github.sh
+++ up_github.sh
@@ -4,7 +4,7 @@ git branch
 git checkout newbranch
 git add .
 git commit -m "note"
-git checkout gh-pages
+git checkout master
 git merge newbranch 
 git push  origin gh-pages 
 git branch -D newbranch 

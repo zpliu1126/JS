787bc8394cfd2790f1e895a336e915a2753bd871
zpliu1126 <1944532210@qq.com>
2019-01-23 11:40:11 +0800

    note

---
+++ up_github.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+git branch newbranch 
+git branch
+git checkout newbranch
+git add .
+git commit -m "note"
+git checkout master 
+git merge newbranch 
+git push  origin master 
+git branch -D newbranch 
